9fc7131b4e27c1049f70d6ddd266054228973775
brsoaresc <bruunascassola@gmail.com>
2024-10-16 20:37:14 +0100

    Update ft_abs.h

--- Piscine Reloaded/ex22/ft_abs.h
+++ Piscine Reloaded/ex22/ft_abs.h
@@ -13,6 +13,6 @@
 #ifndef FT_ABS_H
 # define FT_ABS_H
 
-# define ABS(value) ((valeu) * ((value) >= 0) - (value) * ((value) < 0))
+# define ABS(value) ((value) * ((value) >= 0) - (value) * ((value) < 0))
 
 #endif
