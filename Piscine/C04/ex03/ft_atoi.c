8854243bc784c90b2054d65066c5f73d0a5270a0
brsoaresc <bruunascassola@gmail.com>
2024-10-17 21:17:01 +0100

    Update ft_atoi.c

--- Piscine/C04/ex03/ft_atoi.c
+++ Piscine/C04/ex03/ft_atoi.c
@@ -12,27 +12,28 @@
 
 int	ft_atoi(char *str)
 {
-	int	a;
-	int	b;
-	int	c;
+	int	a; //integrador para percorrer cada caracter da string
+	int	b; //integrador para indicar o sinal do numero
+	int	c; //integrador para armazenar o resultado final
 
-	a = 0;
-	b = 1;
-	c = 0;
-	while ((str[a] >= '\t' && str[a] <= '\r') || str[a] == ' ')
+	a = 0; //comeca a percorrer da posicao 0
+	b = 1; //assume que inicialmente o numero e positivo
+	c = 0; //inicializa o resultado com 0
+	while ((str[a] >= '\t' && str[a] <= '\r') || str[a] == ' ') //pula caracteres de controle e espaco em branco
 		a++;
-	while (str[a] == '-' || str[a] == '+')
+	while (str[a] == '-' || str[a] == '+') //verifica o sinal
 	{
-		if (str[a] == '-')
-			b *= -1;
+		if (str[a] == '-') //se for negativo muda o sinal para negativo, agora int nao mais char
+			b *= -1; //b = b * -1, qualquer numero vezes -1 o torna negativo
 		a++;
 	}
-	while (str[a] >= '0' && str[a] <= '9')
+	while (str[a] >= '0' && str[a] <= '9') //aqui e onde a convercao ocorre, enquanto houver numeros
 	{
-		c = (c * 10) + (str[a] - '0');
+		c = (c * 10) + (str[a] - '0'); //converte o caracter para inteiro '0' e 48, e '1' e 49, subtraindo fica 1 
+		//ex: 12 ... (1 + '0') + (0* 10) = c = 1 / (2 + '0') + (1* 10) = c = 12 
 		a++;
 	}
-	return (c * b);
+	return (c * b); //retorna o valor final * o sinal armazenado em b 
 }
 /*
 #include <stdio.h>
