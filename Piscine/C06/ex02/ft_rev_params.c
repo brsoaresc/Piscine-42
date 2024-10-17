8a37480d469c2cc11b482e0e15fb4610d8986c87
brsoaresc <bruunascassola@gmail.com>
2024-10-17 20:41:35 +0100

    Update ft_rev_params.c

--- Piscine/C06/ex02/ft_rev_params.c
+++ Piscine/C06/ex02/ft_rev_params.c
@@ -10,28 +10,30 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+//a funcao imprime so os parametros passados do ultimo par o primeiro
+
 #include <unistd.h>
 
 int	main(int argc, char *argv[])
 {
-	int	a;
-	int	b;
+	int	a; //integrador de argumentos
+	int	b; //integrador dos caracteres de cada argumentos
 
-	a = argc -1;
-	b = 0;
-	if (argc > 1)
+	a = argc -1; //inicia no ultimo argumento passado
+	b = 0; //comeca a contar da posicao 0
+	if (argc > 1) //verifica se ha mais de um argumento
 	{
-		while (a > 0)
+		while (a > 0) //percorre os argumentos de tras par frente, comencando com argv -1
 		{
-			b = 0;
-			while (argv[a][b] != '\0')
+			b = 0; //antes de comecar cada novo argumento reseta o b para 0
+			while (argv[a][b] != '\0') //percorre cada caracter da string argv[a], ate o \0 argv[b]
 			{
 				write (1, &argv[a][b], 1);
-				b++;
+				b++; //para para o proximo caracter do argumento atual
 			}
-			write (1, "\n", 1);
-			a--;
+			write (1, "\n", 1); //quebra de linha para separar os argumentos
+			a--; //move para o arguemento anterior, voltando do ultimo para o primeiro
 		}
 	}
-	return (0);
+	return (0); //se o numero de argumetos for menor que 0, retorna 0
 }
