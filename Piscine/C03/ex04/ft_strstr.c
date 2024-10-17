2de6c0c1d5104f46e812fc7af3ef92967d3ff70e
brsoaresc <bruunascassola@gmail.com>
2024-10-17 21:37:56 +0100

    Update ft_strstr.c

--- Piscine/C03/ex04/ft_strstr.c
+++ Piscine/C03/ex04/ft_strstr.c
@@ -22,14 +22,18 @@ char	*ft_strstr(char *str, char *to_find)
 	{
 		b = 0;
 		while (str[a + b] == to_find[b] && str[a + b] != '\0')
+//str[a+b] a representa a posicao atual de str onde a comparcao com to_find comeca
+//b representa subtring de to_find, compara as duas simultaneamente
 		{
 			if (to_find[b + 1] == '\0')
-				return (&str[a]);
+//acessa o proximo caracter de to_find, caracter seguinte ao ultimo caracter comparado e verifica se e igual a nulo
+				return (&str[a]); 
+//retorna um ponteiro para o indice a en str(ou seja, o inicio da substring encontrada dentro de str)
 			b++;
 		}
 		a++;
 	}
-	return (0);
+	return (0); //retorna 0, se a substring nao for encontrada
 }
 /*
 #include <stdio.h>
