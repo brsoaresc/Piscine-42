7af8572d36462802630975ab20671eb84bf09600
Bruna Cassola <bruunascassola@gmail.com>
2024-10-20 13:02:13 +0100

    Update ft_point.h

--- Piscine Reloaded/ex23/ft_point.h
+++ Piscine Reloaded/ex23/ft_point.h
@@ -10,8 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_POINT_H
-# define FT_POINT_H
+#ifndef FT_POINT_H    //verifica se o identificador FT_POINT_H ainda não foi definido
+# define FT_POINT_H   //define o identificador FT_POINT_H
 
 typedef struct t_point
 {
@@ -19,4 +19,11 @@ typedef struct t_point
 	int	y;
 }	t_point;
 
-#endif
+#endif //fecha o header guard, finalizando a verificação que começou com #ifndef
+
+//explicacao do comando typedef struct t_point
+//typedef usado para criar um apelido para um tipo de dado. Nesse caso, estamos criando um apelido para a estrutura que definiremos
+//struct t_point define uma estrutura chamada t_point. Uma estrutura (struct) é um tipo de dado que pode armazenar múltiplos valores de diferentes tipos em um só objeto,
+//a estrutura armazena dois valores inteiros, x e y, representando as coordenadas de um ponto
+//int x; int y; são os campos da estrutura, x e y são inteiros que armazenarão as coordenadas do ponto
+//} t_point; o typedef permite que a estrutura seja referenciada diretamente como t_point, em vez de struct t_point. Isso facilita a declaração de variáveis usando essa estrutura
